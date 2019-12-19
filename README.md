2372d7e0a810e5052d87efaf39d432e581ea1c5f
Victor Gianvechio <victor.gianvecchio@live.com>
2019-12-19 13:43:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## React-Electron Boilerplate
+
+`yarn dev` - run developer mode
+
+`yarn electron-build` - build dist project
---
+++ public/electron.js
@@ -0,0 +1,52 @@
+const { BrowserWindow, app } = require('electron');
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1366,
+    height: 768,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    // frame: false,
+    // transparent: false,
+    // resizable: false
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.resolve(__dirname, '..', 'build', 'index.html')}`
+  );
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
